f92b7dab6f2d0619b9ff7e8ea6ebf48552b99056
wentun <1132059466@qq.com>
2020-04-18 17:00:58 +0800

    生成自定义路由组件

--- product/src/components/routerchild/2.jsx
+++ product/src/components/routerchild/2.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Index2 extends React.Component{
     render(){
+        console.log('子路由接收数据',this.props)
         return (
             <div >
               第二个子路由
--- product/src/components/routerchild/touter.jsx
+++ product/src/components/routerchild/touter.jsx
@@ -33,11 +33,21 @@ export default function ChildRouter(){
                 <li><NavLink to="/childrouter/Index1" activeClassName="router-active">一</NavLink></li>
                 <li><NavLink to="/childrouter/Index2" activeClassName="router-active">二</NavLink></li>
                 <li><NavLink to="/childrouter/Index3" activeClassName="router-active">三</NavLink></li>
+                <li><NavLink to="/childrouter/Index4" activeClassName="router-active">跳转render函数式组件</NavLink></li>
             </ul>
             <div>
                 <Route path="/childrouter/Index1" component={Index1}></Route>
                 <Route path="/childrouter/Index2" component={Index2}></Route>
                 <Route path="/childrouter/Index3" component={Index3}></Route>
+                <Route path="/childrouter/Index4" render={(props)=>{
+                    console.log(props)
+                    return(
+                        <div>
+                            这是render函数生成的组件
+                            {/* 这是props传送的数据{props.match} */}
+                        </div>
+                    )
+                }}></Route>
             </div>
         </Router>
     )
