6f117df255e7fc1a693fd7bd8d5000b8c496b81a
wentun <1132059466@qq.com>
2020-05-13 22:19:53 +0800

    练习

--- product/src/components/refs/reftest.jsx
+++ product/src/components/refs/reftest.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class CustomTextInput extends React.Component {
     constructor(props) {
         super(props);
-    
+        this.state = {
+            name:'组件内部数据'
+        }
         this.textInput = null;
     
         this.setTextInputRef = (e) => {
@@ -19,13 +21,19 @@ class CustomTextInput extends React.Component {
             //我的
             // this.setTextInputRef.current.focus()
         };
+
     }
-  
+    
     componentDidMount() {
         // 组件挂载后，让文本框自动获得焦点
         this.focusTextInput();
     }
-  
+    
+    showText(){
+        console.log(" input输入框数据" ,this.refs.input.value)
+        console.log(this.state.name)
+    }
+
     render() {
         // 使用 `ref` 的回调函数将 text 输入框 DOM 节点的引用存储到 React
         // 实例上（比如 this.textInput）
@@ -33,6 +41,8 @@ class CustomTextInput extends React.Component {
             <div>
                 <input type="text" ref={this.setTextInputRef}/>
                 <input type="button" value="Focus the text input" onClick={this.focusTextInput}/>
+                <input type="text" ref="input" />
+                <input type="button" value="获取ref" onClick={()=>this.showText()}/>
             </div>
         );
     }
